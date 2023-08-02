1529b4f459b9de252c5f77bdbf45eeaac04907c6
Christopher Wiley <wiley@applied.co>
2023-08-02 09:40:47 -0700

    Add leader j implementation
    This finds instances of the word under the cursor in the codebase.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,18 @@ syntax on
 "  https://github.com/neovim/neovim/wiki/FAQ#how-can-i-use-true-color-in-the-terminal
 " set termguicolors
 " colorscheme monokai
+nnoremap <silent> <C-j> :RG <C-r><C-w><CR>
+
+function! RipgrepFzf(query, fullscreen)
+  let command_fmt = 'rg --column --line-number --no-heading --color=always --smart-case -- %s || true'
+  let initial_command = printf(command_fmt, shellescape(a:query))
+  let reload_command = printf(command_fmt, '{q}')
+  let spec = {'options': ['--disabled', '--query', a:query, '--bind', 'change:reload:'.reload_command]}
+  let spec = fzf#vim#with_preview(spec, 'right', 'ctrl-/')
+  call fzf#vim#grep(initial_command, 1, spec, a:fullscreen)
+endfunction
+
+command! -nargs=* -bang RG call RipgrepFzf(<q-args>, <bang>0)
 ]])
 
 vim.cmd([[
